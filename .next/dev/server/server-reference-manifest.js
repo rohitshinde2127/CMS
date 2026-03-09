afb9081d4a0c72940cacbe22d1190412d3915b26
kunal <kunalgaikwad0201@gmail.com>
2026-03-09 09:16:36 +0530

    fetch url updated

--- .next/dev/server/server-reference-manifest.js
+++ .next/dev/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"wZ7w6jZZC0c59A3/IyJkrk7wL0nkwoTaXr+/4Q6Y4iM=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"WpxnFWe9wSu7iZmyoxdrCUGmYFVkH1ShKjPKtJAwrH8=\"\n}"
